23ae8b63283c05e664b1d43072f7824e54d3ccfa
kkeulemans <78099351+kkeulemans@users.noreply.github.com>
2022-04-29 11:31:52 +0200

    g

--- src/main/java/com/example/zorgappfinal/controller/UserController.java
+++ src/main/java/com/example/zorgappfinal/controller/UserController.java
@@ -62,7 +62,6 @@ public class UserController {
     @PostMapping(value = "")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto dto) {
 
-
         String newUsername = userService.createUser(dto);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{username}")
